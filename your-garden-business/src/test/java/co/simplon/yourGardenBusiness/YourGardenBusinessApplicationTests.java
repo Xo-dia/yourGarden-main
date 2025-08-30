38a75ea753497b24577cea414e7edeb7e9ae6d2f
Xodia <fjldumele@gmail.com>
2025-08-30 02:14:02 +0200

    first junit test

---
+++ your-garden-business/src/test/java/co/simplon/yourGardenBusiness/YourGardenBusinessApplicationTests.java
@@ -0,0 +1,10 @@
+package co.simplon.yourGardenBusiness;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BuildPassTest {
+    @Test void passes() {
+        assertTrue(true);
+    }
+}
